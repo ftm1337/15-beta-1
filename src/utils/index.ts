3fc52a19e6f6633c47d3863e139aab6d6b1bfc42
ftm1337 <ftm1337@protonmail.com>
2022-05-28 16:59:49 -0400

    Alpha Release 3

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -18,15 +18,15 @@ export function isAddress(value: any): string | false {
 }
 
 const ETHERSCAN_PREFIXES: { [chainId in ChainId]: string } = {
-  62621: 'mtvscout.com',
-  3: 'ropsten.etherscan.io',
-  4: 'rinkeby.etherscan.io',
-  5: 'goerli.etherscan.io',
-  42: 'kovan.etherscan.io'
+  62621: '',
+  3: 'ropsten.',
+  4: 'rinkeby.',
+  5: 'goerli.',
+  42: 'kovan.'
 }
 
 export function getEtherscanLink(chainId: ChainId, data: string, type: 'transaction' | 'token' | 'address'): string {
-  const prefix = `https://`
+  const prefix = `https://${ETHERSCAN_PREFIXES[chainId] || ETHERSCAN_PREFIXES[62621]}`
 
   switch (type) {
     case 'transaction': {
