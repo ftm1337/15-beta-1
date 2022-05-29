1c44fc2ae20dc6b17f94c68216092907775221d1
ftm1337 <ftm1337@protonmail.com>
2022-05-29 11:03:09 -0400

    Use Default List fro process.env

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://mtv.guru/vax-list/vax-list.json'
+export const DEFAULT_TOKEN_LIST_URL = process.env.REACT_APP_SWAP_TOKEN_LIST_URL || ''
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  'https://mtv.guru/vax-list/vax-list.json'
+  DEFAULT_TOKEN_LIST_URL
 ]
