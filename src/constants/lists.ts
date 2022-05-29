2b0e5f63e926397c93d1c5c6ba275790524166c3
ftm1337 <ftm1337@protonmail.com>
2022-05-29 10:37:37 -0400

    Set Default list to HTTPS

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,6 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'http://mtv.guru/vax-list/vax-list.json'
+export const DEFAULT_TOKEN_LIST_URL = 'https://mtv.guru/vax-list/vax-list.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  'http://mtv.guru/vax-list/vax-list.json'
+  'https://mtv.guru/vax-list/vax-list.json'
 ]
