7937ffe427cb32a6b7f9967d7263cf32601209ca
ftm1337 <ftm1337@protonmail.com>
2022-05-29 11:29:18 -0400

    tokenlist arrgggghhgit push https://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@github.com/ftm1337/15-alpha-1.git

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,6 +1,12 @@
 // the Uniswap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = process.env.REACT_APP_SWAP_TOKEN_LIST_URL || ''
+export const DEFAULT_TOKEN_LIST_URL = 'https://mtv.guru/vax-list/vax-list.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
-  DEFAULT_TOKEN_LIST_URL
+  DEFAULT_TOKEN_LIST_URL,
+  'https://raw.githubusercontent.com/mtvguru/vax-list/main/docs/vax-list.json'
+  'https://www.coingecko.com/tokens_list/uniswap/defi_100/v_0_0_0.json',
+  'https://app.tryroll.com/tokens.json',
+  'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json',
+  'https://defiprime.com/defiprime.tokenlist.json',
+  'https://umaproject.org/uma.tokenlist.json'
 ]
